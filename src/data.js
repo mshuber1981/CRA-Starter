3f05e7d42059b1c7c43369a57a41fe84cbe91b20
Michael Huber <michael.huber1981@outlook.com>
2022-01-18 08:24:30 -0600

    Adding nav links data

---
+++ src/data.js
@@ -0,0 +1,33 @@
+// NavBar/Sidbar links (Looks crowded after 6)
+export const links = [
+  {
+    ID: "1",
+    name: "Section 1",
+    link: "#",
+  },
+  {
+    ID: "2",
+    name: "Section 2",
+    link: "#",
+  },
+  {
+    ID: "3",
+    name: "Section 3",
+    link: "#",
+  },
+  {
+    ID: "4",
+    name: "Section 4",
+    link: "#",
+  },
+  {
+    ID: "5",
+    name: "Section 5",
+    link: "#",
+  },
+  {
+    ID: "6",
+    name: "Route Example",
+    link: "/Route-1",
+  },
+];
