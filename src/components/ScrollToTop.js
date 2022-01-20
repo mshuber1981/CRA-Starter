c693bf9d76921599d44670c20d8667bac4c3f087
Michael Huber <michael.huber1981@outlook.com>
2022-01-20 11:44:47 -0600

    Adding guides link comment

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-
+// https://v5.reactrouter.com/web/guides/scroll-restoration
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
