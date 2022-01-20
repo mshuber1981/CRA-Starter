8e4214a4e4d7948df2b44392d09349fe244cc584
Michael Huber <michael.huber1981@outlook.com>
2022-01-20 11:45:01 -0600

    Updating links data structure

--- src/data.js
+++ src/data.js
@@ -1,33 +1,86 @@
-// NavBar/Sidbar links (Looks crowded after 6)
+/**
+ * Navigation Logo
+ */
+import navLogo from "./media/logo.svg";
+export const Logo = navLogo;
+
+/**
+ * Colors
+ */
+export const active = "";
+
+/**
+ * NavBar/Sidbar pages and links
+ */
+// export const links = [
+//   {
+//     ID: "1",
+//     name: "Home",
+//     link: "Home",
+//   },
+//   {
+//     ID: "2",
+//     name: "Section 1",
+//     link: "Section-1",
+//   },
+//   {
+//     ID: "3",
+//     name: "Section 2",
+//     link: "Section-2",
+//   },
+//   {
+//     ID: "4",
+//     name: "Section 3",
+//     link: "Section-3",
+//   },
+//   {
+//     ID: "5",
+//     name: "Route Example",
+//     link: "/Route-1",
+//   },
+// ];
+
 export const links = [
   {
-    ID: "1",
-    name: "Section 1",
-    link: "#",
+    page: "Home",
+    route: "/",
+    links: [
+      {
+        id: "1",
+        name: "Home",
+        to: "Home",
+      },
+      {
+        id: "2",
+        name: "Section 1",
+        to: "Section-1",
+      },
+      {
+        id: "3",
+        name: "Section 2",
+        to: "Section-2",
+      },
+      {
+        id: "4",
+        name: "Section 3",
+        to: "Section-3",
+      },
+      {
+        id: "5",
+        name: "Route Example",
+        to: "/Route-1",
+      },
+    ],
   },
   {
-    ID: "2",
-    name: "Section 2",
-    link: "#",
-  },
-  {
-    ID: "3",
-    name: "Section 3",
-    link: "#",
-  },
-  {
-    ID: "4",
-    name: "Section 4",
-    link: "#",
-  },
-  {
-    ID: "5",
-    name: "Section 5",
-    link: "#",
-  },
-  {
-    ID: "6",
-    name: "Route Example",
-    link: "/Route-1",
+    page: "Route1Example",
+    route: "/Route-1",
+    links: [
+      {
+        id: "1",
+        name: "Home",
+        to: "/",
+      },
+    ],
   },
 ];
