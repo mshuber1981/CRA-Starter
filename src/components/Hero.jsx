a6258292b55e12f40f8b96ff92d58a97a9b3a866
Michael Huber <michael.huber1981@outlook.com>
2022-01-30 09:05:16 -0600

    Updating styles, adding hero imgs for lg screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,17 @@ import { Link } from "react-scroll";
 import { FaChevronCircleDown } from "react-icons/fa";
 // Media
 import Logo from "../media/logo.svg";
+import HeroLight from "../media/hero-light.jpg";
+import HeroDark from "../media/hero-dark.jpg";
 // Components
-import { Title } from "./globalStyledComponents";
+import { Spin } from "./globalStyledComponents";
 
 const StyledHero = styled.header`
   position: relative;
   display: grid;
   place-items: center;
+  max-width: 1920px;
+  margin: 0 auto;
   min-height: calc(100vh - var(--nav-height));
 
   &::before {
@@ -20,7 +24,10 @@ const StyledHero = styled.header`
     left: 0;
     width: 100%;
     height: 100%;
-    background: linear-gradient(135deg, var(--primary), var(--dark));
+    background: ${({ theme }) =>
+      theme.name === "light"
+        ? "linear-gradient(135deg, var(--primary), var(--light))"
+        : "linear-gradient(135deg, var(--primary), var(--dark))"};
     z-index: -2;
   }
 
@@ -48,6 +55,14 @@ const StyledHero = styled.header`
   }
 
   @media screen and (min-width: 800px) {
+    &::before {
+      background: ${({ theme }) =>
+        theme.name === "light"
+          ? `url(${HeroLight}) no-repeat center center fixed`
+          : `url(${HeroDark}) no-repeat center center fixed`};
+      background-size: cover;
+    }
+
     .hero-center {
       grid-template-columns: repeat(2, 1fr);
 
@@ -57,6 +72,7 @@ const StyledHero = styled.header`
 
         h1 {
           font-size: 3rem;
+          line-height: 1.25;
         }
       }
 
@@ -70,6 +86,12 @@ const StyledHero = styled.header`
       font-size: 3rem;
     }
   }
+
+  @media (prefers-reduced-motion: no-preference) {
+    .hero-img {
+      animation: ${Spin} infinite 20s linear;
+    }
+  }
 `;
 
 export default function Hero({ section1 }) {
@@ -77,7 +99,11 @@ export default function Hero({ section1 }) {
     <StyledHero>
       <div className="hero-center">
         <article className="hero-info">
-          <Title />
+          <h1>
+            Sample
+            <br />
+            Call To Action
+          </h1>
         </article>
         <article className="hero-images">
           <img src={Logo} alt="React Logo" className="hero-img" />
