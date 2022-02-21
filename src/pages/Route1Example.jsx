1655bb8d6f46072a3fe7592eb8e782b25fb00663
Michael Huber <michael.huber1981@outlook.com>
2022-02-21 07:58:54 -0600

    Adding Sidebar to Route1 page

--- src/pages/Route1Example.jsx
+++ src/pages/Route1Example.jsx
@@ -1,10 +1,11 @@
-// import NavBar from "../components/NavBar";
-import { NavBar } from "../components/globalStyledComponents";
+// Components
+import { NavBar, Sidebar } from "../components/globalStyledComponents";
 
 export default function Route1Example({ route1 }) {
   return (
     <>
       <NavBar pageLinks={route1.links} />
+      <Sidebar pageLinks={route1.links} />
       <main>
         <section className="section section-center">
           <h1>h1</h1>
