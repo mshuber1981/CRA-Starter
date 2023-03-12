041b5f5c8f99f9812f3f46664754ebc956b200db
mshuber1981 <michael.huber1981@outlook.com>
2023-03-12 11:25:12 -0500

    Closes #28 testing Suspense/lazy

---
+++ src/components/Picture.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+// Images
+import mobile from "../media/mobile.png";
+import mobile2x from "../media/mobile@2x.png";
+import tablet from "../media/tablet.png";
+import tablet2x from "../media/tablet@2x.png";
+import desktop from "../media/desktop.png";
+import desktop2x from "../media/desktop@2x.png";
+
+const Picture = () => {
+  return (
+    <picture>
+      <source media="(max-width: 420px)" srcSet={`${mobile}, ${mobile2x}`} />
+      <source media="(max-width: 820px)" srcSet={`${tablet}, ${tablet2x}`} />
+      <source srcSet={`${desktop}, ${desktop2x}`} />
+      <img src={desktop} alt="Desktop computer icon" loading="lazy" />
+    </picture>
+  );
+};
+
+export default Picture;
--- src/components/ResponsiveImages.jsx
+++ src/components/ResponsiveImages.jsx
@@ -1,22 +1,35 @@
-import React from "react";
-// Images
-import desktop from "../media/desktop.png";
-import desktop2x from "../media/desktop@2x.png";
-import tablet from "../media/tablet.png";
-import tablet2x from "../media/tablet@2x.png";
-import mobile from "../media/mobile.png";
-import mobile2x from "../media/mobile@2x.png";
+import React, { Suspense } from "react";
+import styled from "styled-components";
+// Icons
+import { Icon } from "@iconify/react";
+// Components
+const Images = React.lazy(() => import("./Picture"));
+
+const StyledDiv = styled.div`
+  width: 40rem;
+  max-width: 95vw;
+  margin: 1rem auto 1rem auto;
+
+  .icon {
+    font-size: 10rem;
+    margin: 0 auto;
+  }
+
+  picture {
+    width: 100%;
+    height: auto;
+  }
+`;
 
 const ResponsiveImages = () => {
   return (
-    <>
-      <picture className="mb-3">
-        <source media="(max-width: 420px)" srcSet={`${mobile}, ${mobile2x}`} />
-        <source media="(max-width: 820px)" srcSet={`${tablet}, ${tablet2x}`} />
-        <source srcSet={`${desktop}, ${desktop2x}`} />
-        <img src={desktop} alt="Desktop computer icon" />
-      </picture>
-    </>
+    <StyledDiv className="d-flex flex-column justify-content-center">
+      <Suspense
+        fallback={<Icon icon="mdi:picture-360-outline" className="icon" />}
+      >
+        <Images />
+      </Suspense>
+    </StyledDiv>
   );
 };
 
