f147d08a17a471c94ec7c1b45e939b53ca8ae411
mshuber1981 <michael.huber1981@outlook.com>
2023-06-08 07:53:59 -0500

    Adding devtools ext init

--- src/store.js
+++ src/store.js
@@ -3,8 +3,12 @@ import { configureStore } from "@reduxjs/toolkit";
 //Reducers
 import homeReducer from "./pages/homeSlice";
 
-export const store = configureStore({
-  reducer: {
-    home: homeReducer,
+export const store = configureStore(
+  {
+    reducer: {
+      home: homeReducer,
+    },
   },
-});
+  // https://github.com/reduxjs/redux-devtools/tree/main/extension#1-with-redux
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
