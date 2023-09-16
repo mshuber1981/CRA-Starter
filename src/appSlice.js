563c021daa349e80b22b544e421a82baa0cc1a04
mshuber1981 <michael.huber1981@outlook.com>
2023-09-16 08:54:56 -0500

    Converting app context to redux

--- src/appSlice.js
+++ src/appSlice.js
@@ -1,22 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  mode: "light",
+  mode: null,
+  isExpanded: false,
+};
+
+const setStoredTheme = (theme) => localStorage.setItem("theme", theme);
+const setTheme = (theme) => {
+  if (
+    theme === "auto" &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-bs-theme", theme);
+  }
 };
 
 export const appSlice = createSlice({
   name: "app",
   initialState,
   reducers: {
+    closeExpanded: (state) => {
+      state.isExpanded = false;
+    },
+    toggleExpanded: (state) => {
+      state.isExpanded = !state.isExpanded;
+    },
     setMode: (state, action) => {
       state.mode = action.payload;
+      setTheme(state.mode);
     },
     toggleMode: (state) => {
       state.mode === "light" ? (state.mode = "dark") : (state.mode = "light");
+      setTheme(state.mode);
+      setStoredTheme(state.mode);
     },
   },
 });
 
-export const { setMode, toggleMode } = appSlice.actions;
+export const selectMode = (state) => state.app.mode;
+
+export const selectIsExpanded = (state) => state.app.isExpanded;
+
+export const { closeExpanded, setMode, toggleMode, toggleExpanded } =
+  appSlice.actions;
 
 export default appSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,14 +1,10 @@
 // https://redux.js.org/tutorials/fundamentals/part-8-modern-redux#using-configurestore
 import { configureStore } from "@reduxjs/toolkit";
 //Reducers
-import homeReducer from "./pages/homeSlice";
+import appReducer from "./appSlice";
 
-export const store = configureStore(
-  {
-    reducer: {
-      home: homeReducer,
-    },
+export const store = configureStore({
+  reducer: {
+    app: appReducer,
   },
-  // https://github.com/reduxjs/redux-devtools/tree/main/extension#1-with-redux
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+});
