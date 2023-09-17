c5a80ac20b2e96352486ab169ab1bddfb81221db
mshuber1981 <michael.huber1981@outlook.com>
2023-09-17 15:03:25 -0500

    Adding Container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // Components
-import { Button, Spinner } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 // Data
 import { homeRouteName } from "../data";
 // Utils
@@ -14,16 +14,18 @@ const Home = () => {
 
   return (
     <section>
-      <h1>h1</h1>
-      <h2>h2</h2>
-      <h3>h3</h3>
-      <h4>h4</h4>
-      <h5>h5</h5>
-      <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-      <br />
-      <Button>Primary</Button>
+      <Container>
+        <h1>h1</h1>
+        <h2>h2</h2>
+        <h3>h3</h3>
+        <h4>h4</h4>
+        <h5>h5</h5>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+        <br />
+        <Button>Primary</Button>
+      </Container>
     </section>
   );
 };
--- src/pages/Route1.jsx
+++ src/pages/Route1.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // Routing
 import { useLocation } from "react-router-dom";
 // Components
+import { Container } from "react-bootstrap";
 import Title from "../components/Title";
 // Utils
 import { updateTitle } from "../util";
@@ -16,9 +17,11 @@ const Route1 = () => {
 
   return (
     <section>
-      <Title text={"Route 1 h1 Title"} />
-      <br />
-      <Title size={"h2"} text={"Route 1 h2 Title"} />
+      <Container>
+        <Title text={"Route 1 h1 Title"} />
+        <br />
+        <Title size={"h2"} text={"Route 1 h2 Title"} />
+      </Container>
     </section>
   );
 };
