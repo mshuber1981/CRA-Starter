e9926b2add7a0d1ef29004f7720f4a5f5a4d806d
mshuber1981 <michael.huber1981@outlook.com>
2023-09-17 13:58:36 -0500

    Updating data

--- src/data.js
+++ src/data.js
@@ -1,14 +1,32 @@
-/* Colors
- **************************************************************
- */
+// Pages
+import Route1 from "./pages/Route1";
+
+// Primary color
 export const primary = "#61DBFB";
 
-/* Navigation Logo
- **************************************************************
- */
+// Navbar logo
 export const navLogo = undefined;
 
-/* GitHub
- **************************************************************
- */
-export const githubUsername = "mshuber1981";
+// Navbar routes
+export const navRoutes = [
+  { id: 0, name: "Route 1", route: "/Route1", page: <Route1 /> },
+];
+
+// Social media
+export const socials = [
+  {
+    id: 0,
+    url: "https://github.com/mshuber1981",
+    aria: "Check out my GitHub profile.",
+    icon: "fa:github",
+  },
+  {
+    id: 1,
+    url: "https://www.freecodecamp.org/mshuber1981",
+    aria: "Check out my Free Code Camp profile.",
+    icon: "fa-brands:free-code-camp",
+  },
+];
+
+// GitHub username
+export const gitHubUsername = "mshuber1981";
