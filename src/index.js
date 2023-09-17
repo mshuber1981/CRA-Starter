9bde245955537719c65c39bcfe3c98af4e30dfcc
mshuber1981 <michael.huber1981@outlook.com>
2023-09-17 10:53:02 -0500

    Removing old comments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// https://redux.js.org/tutorials/fundamentals/part-5-ui-react#passing-the-store-with-provider
 import { Provider } from "react-redux";
 import { store } from "./store";
-// https://create-react-app.dev/docs/adding-bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
-// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
