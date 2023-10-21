23a7d4b1768c870dcb252dac0fb8701a6325184a
mshuber1981 <michael.huber1981@outlook.com>
2023-10-21 09:24:22 -0500

    Updating global styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,6 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-// Data
-import { primary } from "../data";
 
 const GlobalStyles = createGlobalStyle`
 /*
@@ -9,12 +7,7 @@ Variables
 ===============
 */
 :root {
-  --primary-color: ${primary};
-  --dark-text-color: #212529;
-  --light-text-color: #dee2e6;
-  --hover-color: rgba(33, 37, 41, 0.5);
   --nav-height: 55px;
-  --transition: all 0.3s linear;
 }
 
 /*
@@ -30,62 +23,17 @@ section {
   margin: 0.5rem 0;
 }
 
-/* Navbar */
-.navbar {
-  height: var(--nav-height);
-  background: var(--primary-color);
-
-  .container {
-    background: var(--primary-color);
-
-    .nav-img {
-      background: var(--bs-body-color);
-    }
-
-    .nav-link {
-      color: var(--dark-text-color);
-
-      &:hover {
-        color: var(--hover-color);
-      }
-    }
-
-    .nav-link.active {
-      font-weight: bold;
-    }
-  }
-}
-
-/* Buttons */
-:not(.btn-check) + .btn:active {
-  background-color: var(--bs-emphasis-color);
-  border: 1px solid var(--primary-color);
-  color: var(--bs-body-bg);
-}
-
-.btn-primary {
-  background: var(--primary-color);
-  border: 1px solid var(--primary-color);
-  color: var(--bs-body-color);
-  margin: 0.25rem 0;
-
-  &:hover {
-    background-color: var(--bs-body-bg);
-    border: 1px solid var(--primary-color);
-    color: var(--bs-body-color);
-  }
-}
-
 /* Link Icons */
 .link-icons {
     line-height: 0;
     font-size: 2.25rem;
     margin: 0 1rem;
-    transition: var(--transition);
-    color: var(--dark-text-color);
+    color: ${({ theme }) =>
+      theme.name === "light" ? "var(--bs-dark)" : "var(--bs-light)"};
 
     &:hover {
-      color: var(--hover-color);
+      color: ${({ theme }) =>
+        theme.name === "light" ? "var(--bs-light)" : "var(--bs-dark)"};
     }
   }
 `;
